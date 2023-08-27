865aed9c4d904b80fa47fa8ae42eeb62445a5a62
Minh-Kop <87182623+Minh-Kop@users.noreply.github.com>
2023-08-27 17:35:19 +0700

    First test commit

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 import express from 'express';
-import fetch from 'node-fetch';
 import 'dotenv/config';
+import axios from 'axios';
 const app = express();
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
 const port = process.env.PORT || 3000;
 const environment = process.env.ENVIRONMENT || 'sandbox';
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
-const endpoint_url = environment === 'sandbox' ? 'https://api-m.sandbox.paypal.com' : 'https://api-m.paypal.com';
+const endpoint_url =
+    environment === 'sandbox'
+        ? 'https://api-m.sandbox.paypal.com'
+        : 'https://api-m.paypal.com';
 
 /**
  * Creates an order and returns it as a JSON response.
@@ -24,37 +29,45 @@ const endpoint_url = environment === 'sandbox' ? 'https://api-m.sandbox.paypal.c
  * @returns {object} The created order as a JSON response.
  * @throws {Error} If there is an error creating the order.
  */
-app.post('/create_order', (req, res) => {
-    get_access_token()
-        .then(access_token => {
-            let order_data_json = {
-                'intent': req.body.intent.toUpperCase(),
-                'purchase_units': [{
-                    'amount': {
-                        'currency_code': 'USD',
-                        'value': '100.00'
-                    }
-                }]
-            };
-            const data = JSON.stringify(order_data_json)
+app.post('/create_order', async (req, res) => {
+    try {
+        // const access_token = await get_access_token();
+        // let order_data_json = {
+        //     intent: req.body.intent.toUpperCase(),
+        //     purchase_units: [
+        //         {
+        //             amount: {
+        //                 currency_code: 'USD',
+        //                 value: '100.00',
+        //             },
+        //         },
+        //     ],
+        // };
+        // const url = `${endpoint_url}/v2/checkout/orders`;
+        // const { data } = await axios.post(url, order_data_json, {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         Authorization: `Bearer ${access_token}`,
+        //     },
+        // });
+        // console.log(data);
 
-            fetch(endpoint_url + '/v2/checkout/orders', { //https://developer.paypal.com/docs/api/orders/v2/#orders_create
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${access_token}`
-                    },
-                    body: data
-                })
-                .then(res => res.json())
-                .then(json => {
-                    res.send(json);
-                }) //Send minimal data to client
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send(err)
-        })
+        const url = 'http://127.0.0.1:3001/api/checkout/OD00001';
+        const requestBody = { paymentId: 'PY01' };
+        const access_token =
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imtob2ltaW5odHJhbm5ndXllbkBnbWFpbC5jb20iLCJpYXQiOjE2OTI5NzI3NTgsImV4cCI6MTcwMDc0ODc1OH0.dap_FmSeo_cGWgAaa3N7RMPsNzVyXpbov5ZlaX174V0';
+        const { data } = await axios.post(url, requestBody, {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            },
+        });
+        res.send({
+            id: data.paymentOrderId,
+        });
+    } catch (err) {
+        console.log(err);
+        res.status(500).send(err);
+    }
 });
 
 /**
@@ -70,26 +83,37 @@ app.post('/create_order', (req, res) => {
  * @returns {object} The completed order as a JSON response.
  * @throws {Error} If there is an error completing the order.
  */
-app.post('/complete_order', (req, res) => {
-    get_access_token()
-        .then(access_token => {
-            fetch(endpoint_url + '/v2/checkout/orders/' + req.body.order_id + '/' + req.body.intent, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'Authorization': `Bearer ${access_token}`
-                    }
-                })
-                .then(res => res.json())
-                .then(json => {
-                    console.log(json);
-                    res.send(json);
-                }) //Send minimal data to client
-        })
-        .catch(err => {
-            console.log(err);
-            res.status(500).send(err)
-        })
+app.post('/complete_order', async (req, res) => {
+    try {
+        // const access_token = await get_access_token();
+        // const url = `${endpoint_url}/v2/checkout/orders/${req.body.order_id}/${req.body.intent}`;
+        // const { data } = await axios.post(url, null, {
+        //     headers: {
+        //         'Content-Type': 'application/json',
+        //         Authorization: `Bearer ${access_token}`,
+        //     },
+        // });
+        // res.send(data);
+
+        const url = 'http://127.0.0.1:3001/api/checkout/notifyPaypal';
+        const requestBody = {
+            paymentOrderId: req.body.order_id,
+            orderId: 'OD00001',
+        };
+        const access_token =
+            'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6Imtob2ltaW5odHJhbm5ndXllbkBnbWFpbC5jb20iLCJpYXQiOjE2OTI5NzI3NTgsImV4cCI6MTcwMDc0ODc1OH0.dap_FmSeo_cGWgAaa3N7RMPsNzVyXpbov5ZlaX174V0';
+        const { data } = await axios.post(url, requestBody, {
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            },
+        });
+        console.log(data);
+        res.send(data);
+    } catch (err) {
+        console.log(err.response.data);
+        err = err.response.data;
+        res.status(500).send(err);
+    }
 });
 
 // Helper / Utility functions
@@ -110,23 +134,22 @@ app.get('/script.js', (req, res) => {
 //PayPal Developer YouTube Video:
 //How to Retrieve an API Access Token (Node.js)
 //https://www.youtube.com/watch?v=HOkkbGSxmp4
-function get_access_token() {
-    const auth = `${client_id}:${client_secret}`
-    const data = 'grant_type=client_credentials'
-    return fetch(endpoint_url + '/v1/oauth2/token', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Authorization': `Basic ${Buffer.from(auth).toString('base64')}`
-            },
-            body: data
-        })
-        .then(res => res.json())
-        .then(json => {
-            return json.access_token;
-        })
-}
+const get_access_token = async () => {
+    const auth = `${client_id}:${client_secret}`;
+    const url = `${endpoint_url}/v1/oauth2/token`;
+    const requestBody = {
+        grant_type: 'client_credentials',
+    };
+    const configHeader = {
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            Authorization: `Basic ${Buffer.from(auth).toString('base64')}`,
+        },
+    };
+    const { data } = await axios.post(url, requestBody, configHeader);
+    return data.access_token;
+};
 
 app.listen(port, () => {
-    console.log(`Server listening at http://localhost:${port}`)
-})
+    console.log(`Server listening at http://localhost:${port}`);
+});
